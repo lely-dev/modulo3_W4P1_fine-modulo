a452ef161357443c92bc63c535b57b9c816dec70
elisa carboni <elisacarboni@live.com>
2024-01-18 01:07:36 +0100

    carousel

---
+++ script.js
@@ -0,0 +1,17 @@
+let items = document.querySelectorAll('#first-carousel-line .carousel-item, #carousel-second .carousel-item, #carousel-third .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
